224a06d57b9f9df3c21907dfa29174976c8d835c
jojoflo2022 <ultrarockdaffunda@gmail.com>
2025-02-10 10:35:03 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // App.js
-
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react"; // useEffect をインポート
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation, // useLocation をインポート
+} from "react-router-dom";
 import Header from "./components/Header";
 import NewHomePage from "./components/NewHomePage";
 import Contact from "./components/Contact/Contact";
@@ -17,6 +21,17 @@ import EducationDetail from "./components/SupportDetail/EducationDetail";
 import "./App.css";
 
 function App() {
+  let location = useLocation(); // useLocationフックを使用
+
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "virtualPageView",
+      pagePath: location.pathname + location.search,
+      pageTitle: document.title,
+    });
+  }, [location]); // locationが変わるたびに実行
+
   return (
     <Router>
       <div className="min-h-screen">
--- src/components/NewHomePage.jsx
+++ src/components/NewHomePage.jsx
@@ -1,3 +1,5 @@
+// NewHomePage.jsx
+
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
